2814886e43a41c0fb00ec75516ac1b7947b300cf
sehyoun <lyutvs@gmail.com>
2021-08-11 06:30:07 +0900

    ⚡️ :: 구글 로그인 연동

--- src/main/java/com/example/board/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/example/board/config/auth/LoginUserArgumentResolver.java
@@ -1,2 +1,32 @@
-package com.example.board.config.auth;public class LoginUserArgumentResolver {
+package com.example.board.config.auth;
+
+import com.example.board.config.annotation.LoginUser;
+import com.example.board.dto.auth.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
 }
