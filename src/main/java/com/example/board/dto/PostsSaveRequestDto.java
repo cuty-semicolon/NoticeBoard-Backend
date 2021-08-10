45b87304a7fd25fe723b032e377f195632fcd1ad
sehyoun <lyutvs@gmail.com>
2021-08-11 03:49:58 +0900

    ⚡️ :: 저장 dto

--- src/main/java/com/example/board/dto/PostsSaveRequestDto.java
+++ src/main/java/com/example/board/dto/PostsSaveRequestDto.java
@@ -1,2 +1,32 @@
-package com.example.board.dto;public class PostsSaveRequestDto {
+package com.example.board.dto;
+
+//Entity와 유사하지만, Dto를 추가로 생성. 왜냐하면 Entity클래스를 절대 Request/response로 사용하면 안되기 때문
+
+
+import com.example.board.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
 }
