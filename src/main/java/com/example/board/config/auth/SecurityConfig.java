2df7886949be789ef5fc94e6c4631d3a1a35ad94
sehyoun <lyutvs@gmail.com>
2021-08-11 06:28:11 +0900

    ⚡️ :: 비회원 게시물 조회

--- src/main/java/com/example/board/config/auth/SecurityConfig.java
+++ src/main/java/com/example/board/config/auth/SecurityConfig.java
@@ -1,2 +1,23 @@
-package com.example.board.config.auth;public class SecurityConfig {
+package com.example.board.config.auth;
+
+import com.example.board.domain.user.Role;
+import com.example.board.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable().headers().frameOptions().disable()
+                .and().authorizeRequests().antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.GUEST.name()).anyRequest().authenticated()
+                .and().logout().logoutSuccessUrl("/")
+                .and().oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+    }
 }
